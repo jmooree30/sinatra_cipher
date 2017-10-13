4e672d0ab834891b9be9b2166e97391694c52202
Jacob <jmooree30@gmail.com>
2017-10-12 21:38:22 -0400

    Project Complete

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'if development?
 
-get '/' do 
-    erb :index
+get '/' do
+def caesar_cipher(text, key)
+  ciphered_text = []
+  text.chars.each do |letter|
+    ciphered_letter = letter
+    if letter.match(/^[[:alpha:]]$/)
+      key.times do
+        if ciphered_letter == 'z'
+          ciphered_letter = 'a'
+        elsif ciphered_letter == 'Z'
+          ciphered_letter = 'A'
+        else
+          ciphered_letter = ciphered_letter.next
+        end
+      end
+    end
+    ciphered_text << ciphered_letter
+  end
+  ciphered_text.join('')
+end
+    text = params['text']
+    key = params['key']
+    cipher = caesar_cipher(text,key.to_i)
+    erb :index, :locals => {:text => text, :key => key, :cipher => cipher}
     end 
